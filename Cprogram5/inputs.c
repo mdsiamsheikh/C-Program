db7f87e7a50366c3b679ca879c8feeff1827e67b
mdsiamsheikh <mdsiamsheikh6640@gmail.com>
2024-10-29 17:23:32 +0600

    add inputs.c

---
+++ Cprogram5/inputs.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() 
+
+{
+    char character;
+    printf("Enter your character: ");
+
+    character = getchar();
+
+printf("siam");
+    printf("You entered: %c\n", character);
+}
